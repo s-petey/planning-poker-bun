0964eda8b6baa5af429bb2e0908c05706f07bfce
Sam Peterson <samllama55@gmail.com>
2023-10-01 14:05:51 +0200

    REMOVE: Error thrown.

--- apps/planning-poker-web/src/lib/index.ts
+++ apps/planning-poker-web/src/lib/index.ts
@@ -5,9 +5,5 @@ import type { App } from 'planning-poker-api';
 const url = import.meta.env.VITE_API_REF_URL;
 const port = import.meta.env.VITE_API_PORT;
 
-if (typeof url === 'undefined' && typeof url === 'undefined') {
-	throw new Error('Invalid configuration');
-}
-
 const combinedUrl = `http://${url}:${port}`;
 export const app = edenTreaty<App>(combinedUrl);
