2a0712e85af11568b0a2b119bd025a29d58ed4b9
Sam Peterson <samllama55@gmail.com>
2023-10-01 19:03:13 +0200

    CHANGE: Svelte env.

--- apps/planning-poker-web/src/lib/index.ts
+++ apps/planning-poker-web/src/lib/index.ts
@@ -1,13 +1,16 @@
 import { edenTreaty } from '@elysiajs/eden';
 import type { App } from 'planning-poker-api';
-import * as env from '$env/static/public';
+import { VITE_FULL_API_URL, VITE_API_PORT, VITE_API_REF_URL } from '$env/static/public';
 
 // place files you want to import through the `$lib` alias in this folder.
-let combinedUrl = env?.VITE_FULL_API_URL;
+let combinedUrl = VITE_FULL_API_URL;
 
-if (combinedUrl === undefined) {
-	const url = env?.VITE_API_REF_URL;
-	const port = env?.VITE_API_PORT;
+if (
+	typeof combinedUrl !== 'string' ||
+	(typeof combinedUrl === 'string' && combinedUrl.length === 0)
+) {
+	const url = VITE_API_REF_URL;
+	const port = VITE_API_PORT;
 	combinedUrl = `http://${url}:${port}`;
 }
 
