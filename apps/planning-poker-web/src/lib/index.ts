d22bb0cf47259b0fd6e4608eddb69de1381f4632
Sam Peterson <samllama55@gmail.com>
2023-10-01 14:13:34 +0200

    CHANGE: Test env-vars.

--- apps/planning-poker-web/src/lib/index.ts
+++ apps/planning-poker-web/src/lib/index.ts
@@ -2,8 +2,8 @@ import { edenTreaty } from '@elysiajs/eden';
 import type { App } from 'planning-poker-api';
 
 // place files you want to import through the `$lib` alias in this folder.
-const url = import.meta.env.VITE_API_REF_URL;
-const port = import.meta.env.VITE_API_PORT;
+const url = process?.env?.VITE_API_REF_URL ??import.meta.env.VITE_API_REF_URL;
+const port = process?.env?.VITE_API_PORT ??import.meta.env.VITE_API_PORT;
 
 const combinedUrl = `http://${url}:${port}`;
 export const app = edenTreaty<App>(combinedUrl);
