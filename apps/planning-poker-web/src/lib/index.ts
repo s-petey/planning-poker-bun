af918a9515216b70768c5bc70c4d464ba5e4c78f
Sam Peterson <samllama55@gmail.com>
2023-10-01 18:33:45 +0200

    CHANGE: Remove process in svelte.

--- apps/planning-poker-web/src/lib/index.ts
+++ apps/planning-poker-web/src/lib/index.ts
@@ -2,14 +2,12 @@ import { edenTreaty } from '@elysiajs/eden';
 import type { App } from 'planning-poker-api';
 
 // place files you want to import through the `$lib` alias in this folder.
-let combinedUrl = process?.env?.VITE_FULL_API_URL ?? import.meta.env.VITE_FULL_API_URL;
+let combinedUrl = import.meta.env.VITE_FULL_API_URL;
 
 if (combinedUrl === undefined) {
-	const url = process?.env?.VITE_API_REF_URL ?? import.meta.env.VITE_API_REF_URL;
-	const port = process?.env?.VITE_API_PORT ?? import.meta.env.VITE_API_PORT;
+	const url = import.meta.env.VITE_API_REF_URL;
+	const port = import.meta.env.VITE_API_PORT;
 	combinedUrl = `http://${url}:${port}`;
 }
-
-console.log('combinedUrl: ', combinedUrl)
 
 export const app = edenTreaty<App>(combinedUrl);
