fcac9aaa7527863088ec406377a3582ebaa8a2f7
Sam Peterson <samllama55@gmail.com>
2023-10-01 14:25:43 +0200

    CHANGE: vite config.

--- apps/planning-poker-web/vite.config.ts
+++ apps/planning-poker-web/vite.config.ts
@@ -1,18 +1,21 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
 	return {
-		server: {
-			proxy: {
-				'/socket': {
-					target: `ws://${process.env.VITE_API_REF_URL}:${process.env.VITE_API_PORT}`,
-					ws: true
-				},
+		server:
+			mode === 'development'
+				? {
+						proxy: {
+							'/socket': {
+								target: `ws://${process.env.VITE_API_URL}:${process.env.VITE_API_PORT}`,
+								ws: true
+							},
 
-				'/api': `http://${process.env.VITE_API_REF_URL}:${process.env.VITE_API_PORT}`
-			}
-		},
+							'/api': `http://${process.env.VITE_API_URL}:${process.env.VITE_API_PORT}`
+						}
+				  }
+				: {},
 		plugins: [sveltekit()]
 	};
 });
